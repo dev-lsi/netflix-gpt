06b22d94571a9326d7a53f4d1923789a48573832
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-03-23 15:59:26 +0200

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl text-lime-800 font-bold">
+      Let's build NetflixGPT
+    </div>
+  );
+}
+
+export default App;
